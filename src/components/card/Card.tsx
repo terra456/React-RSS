e5951bbcb4524d63cd0a0b5e1f6c2a7041a63153
terra456 <terra.456@yandex.ru>
2023-03-16 11:57:47 +0500

    fix: cards display

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -10,10 +10,10 @@ class CardBlock extends React.Component {
 
   render() {
     return (
-      <div className=" min-h-90 h-90 lg:h-90 lg:min-h-90 aspect-w-1 aspect-h-1 aspect-none w-full max-w-[26rem] overflow-hidden rounded-md bg-gray-200 shadow-lg group-hover:opacity-75 ">
-        <div className="min-h-60 aspect-w-1 aspect-h-1 h-60 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-60">
+      <div className="min-h-90 h-90 lg:min-h-90 aspect-w-1 aspect-h-1 aspect-none w-full max-w-[26rem] flex-col overflow-hidden rounded-md bg-gray-200 shadow-lg group-hover:opacity-75 sm:h-full ">
+        <div className="min-h-60 aspect-none h-60 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-60">
           <img
-            src={'./assets/images/mini/' + this.info.picture + '.jpg'}
+            src={'./assets/images/full/' + this.info.picture + 'full.jpg'}
             alt={this.info.name.en}
             className="h-full w-full object-cover object-center lg:h-full lg:w-full"
           />
@@ -23,7 +23,7 @@ class CardBlock extends React.Component {
           <h4 className="mt-1 text-sm text-gray-500">{this.info.author.en}</h4>
           <p className="mt-1 text-sm text-gray-500">{this.info.desc}</p>
         </div>
-        <div className="flex items-center justify-items-start py-3 px-3">
+        <div className="flex grow items-center justify-items-start place-self-end py-3 px-3">
           <p className="mr-3">
             <i className="fa fa-thumbs-up mr-1"></i>
             {this.info.likes}
