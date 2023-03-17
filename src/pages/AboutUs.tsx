7cf06866d91145f2bef95df300173614b5ad0f3f
terra456 <terra.456@yandex.ru>
2023-03-17 15:18:37 +0500

    feat: tests for header

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -9,6 +9,7 @@ class AboutUs extends React.Component {
     return (
       <div className="mx-auto max-w-2xl py-5 px-4 sm:py-4 sm:px-6 lg:max-w-7xl lg:px-8 lg:py-8">
         <h1>About Us</h1>
+        <p>you are on the about page</p>
         <p>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex quis ullam accusantium
           molestias deleniti culpa cum perspiciatis dolorem quod nisi incidunt tenetur ducimus
