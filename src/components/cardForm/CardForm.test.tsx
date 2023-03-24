fe35e406985a035df8497a88b9cb488165ab9473
terra456 <terra.456@yandex.ru>
2023-03-24 14:54:42 +0500

    feat: test for the form

---
+++ src/components/cardForm/CardForm.test.tsx
@@ -0,0 +1,81 @@
+import '@testing-library/jest-dom';
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import selectEvent from 'react-select-event';
+import { afterEach } from 'vitest';
+import CardForm, { State } from './CardForm';
+
+afterEach(() => {
+  cleanup();
+});
+
+const handleCardSubmit = (obj: State) => {
+  console.log(obj);
+  render(
+    <div>
+      <p>{(obj.inputDate, obj.file, obj.selectValue, obj.inputName)}</p>
+    </div>
+  );
+};
+
+// window.URL.createObjectURL(File) => {
+//   return File;
+// }
+
+describe('Checkbox render', () => {
+  it('render checkbox input and label with mock values', async () => {
+    render(<CardForm onFormSubmit={handleCardSubmit} />);
+    const input = await screen.getAllByRole('checkbox');
+    expect(await screen.getByText(/Agree/i)).toBeInTheDocument();
+    expect(await screen.getByRole('button')).toBeInTheDocument();
+    expect(input[0]).not.toBeChecked();
+  });
+
+  it('send some information inputs, validate and clear', async () => {
+    window.URL.createObjectURL = (file: Blob) => {
+      return './test/chucknorris.png';
+    };
+    render(<CardForm onFormSubmit={handleCardSubmit} />);
+    const form = await screen.getByTestId('form');
+    const textInputs = await screen.getAllByRole('textbox');
+    fireEvent.change(textInputs[0], {
+      target: { value: 'Name of the card' },
+    });
+    fireEvent.change(textInputs[1], {
+      target: { value: 'Some Description' },
+    });
+    await selectEvent.select(screen.getByRole('combobox'), 'select1');
+    expect(textInputs[0]).toHaveValue('Name of the card');
+    expect(form).toHaveFormValues({
+      name: 'Name of the card',
+      desc: 'Some Description',
+      nameSelection: 'select1',
+    });
+    expect(await screen.getByRole('button')).not.toBeDisabled();
+    await userEvent.click(await screen.getByRole('button'));
+    expect(await screen.getByRole('button')).toBeDisabled();
+    expect(textInputs[0]).toHaveValue('Name of the card');
+    expect(await screen.getByText(/empty/i)).toBeInTheDocument();
+    expect(await screen.getByText(/add any picture/i)).toBeInTheDocument();
+    expect(await screen.getByText(/You must agree the terms/i)).toBeInTheDocument();
+    fireEvent.change(await screen.getByAltText('date'), {
+      target: { value: '2020-05-24' },
+    });
+    const radioInputs = await screen.getAllByRole('radio');
+    const checkInputs = await screen.getAllByRole('checkbox');
+    fireEvent.click(radioInputs[0]);
+    fireEvent.click(checkInputs[1]);
+    fireEvent.click(checkInputs[3]);
+    fireEvent.click(checkInputs[5]);
+    expect(await screen.getByRole('button')).not.toBeDisabled();
+    await fireEvent.change(screen.getByLabelText(/upload/i), {
+      target: {
+        files: [new File(['(⌐□_□)'], 'chucknorris.png', { type: 'image/png' })],
+      },
+    });
+    expect(textInputs[0]).not.toHaveValue('');
+    await userEvent.click(await screen.getByRole('button'));
+    expect(textInputs[0]).toHaveValue('');
+  });
+});
