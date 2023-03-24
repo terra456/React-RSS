5b9636d59f6cd1193685ad0c29af01032f4c9853
terra456 <terra.456@yandex.ru>
2023-03-24 22:57:28 +0500

    fix: mock card data in the test

--- src/components/cards/Cards.test.tsx
+++ src/components/cards/Cards.test.tsx
@@ -1,11 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { describe, expect } from 'vitest';
+import { CardType } from '../../types';
 import Cards from './Cards';
+
+const cardsMock: CardType[] = [
+  {
+    year: '1213',
+    imgSrc: './asstts/123.jpg',
+    author: { en: 'Name Sername' },
+    name: { en: 'string name test' },
+    desc: 'some info abought the picture',
+    likes: '2',
+    dislikes: '1',
+    watch: '3',
+  },
+];
 
 describe('CardsList', () => {
   it('should display name of one of the card', () => {
-    render(<Cards />);
-    expect(screen.getByText('Horsewoman')).toBeDefined();
+    render(<Cards dataList={cardsMock} />);
+    expect(screen.getByText(/abought/i)).toBeDefined();
   });
 });
