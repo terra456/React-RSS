dfc627cbeddafc83263532062a20f7711d0ef8d9
terra456 <terra.456@yandex.ru>
2023-04-07 12:07:23 +0500

    feat: change search

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -1,18 +1,11 @@
 import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
-import { Character, getCharacter, getCharacters } from 'rickmortyapi';
+import { Character, CharacterFilter, getCharacter, getCharacters } from 'rickmortyapi';
 import Cards from '../components/cards/Cards';
 import Modal from '../components/modal/Modal';
 import SearchBar from '../components/searchBar/SearchBar';
 
-const characters = await getCharacters();
-
 function MainPage() {
   const [data, setData]: [
-    Character[] | undefined[],
-    Dispatch<SetStateAction<Character[] | undefined[]>>
-  ] = useState(Array(20));
-
-  const [newData, setNewData]: [
     Character[] | undefined[],
     Dispatch<SetStateAction<Character[] | undefined[]>>
   ] = useState(Array(20));
@@ -28,36 +21,25 @@ function MainPage() {
   const [searchStr, setSearchStr] = useState('');
 
   useEffect(() => {
-    console.log(characters.data.results);
-    if (characters.data.results) {
-      setData(characters.data.results);
-      setNewData(characters.data.results);
-    }
-    if (characters.data.info) {
-      // setPages(characters.data.info.pages);
-      // setCount(characters.data.info.count);
-    }
-  }, []);
+    (async () => {
+      const filters: CharacterFilter = {
+        name: searchStr.toLowerCase(),
+      };
+      const characters = await getCharacters(filters);
+      console.log(characters.data.results);
+      if (characters.data.results) {
+        setData(characters.data.results);
+      }
+      if (characters.data.info) {
+        // setPages(characters.data.info.pages);
+        // setCount(characters.data.info.count);
+      }
+    })();
+  }, [searchStr]);
 
   useEffect(() => {
     setIsOpen(true);
   }, [element]);
-
-  useEffect(() => {
-    if (searchStr && searchStr !== '') {
-      searchStr.toLowerCase();
-      const filteredData = data.filter(({ name, status, species, type, gender }) => {
-        return (
-          name.toLowerCase().includes(searchStr) ||
-          status.toLowerCase().includes(searchStr) ||
-          species.toLowerCase().includes(searchStr) ||
-          type.toLowerCase().includes(searchStr) ||
-          gender.toLowerCase().includes(searchStr)
-        );
-      });
-      setNewData(filteredData);
-    }
-  }, [searchStr, data]);
 
   const handleCardClick = (el: number): void => {
     console.log(el);
@@ -79,7 +61,7 @@ function MainPage() {
   return (
     <div className="bg-white">
       <SearchBar handleSearch={handleSearch} />
-      <Cards dataList={newData} handleClick={handleCardClick} />
+      <Cards dataList={data} handleClick={handleCardClick} />
       <Modal data={element} open={isOpen} handleClose={closeModal} />;
     </div>
   );
