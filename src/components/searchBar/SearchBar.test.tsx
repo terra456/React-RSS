fa42a4879ae76a4cd9d0cda6d0022505c58705ec
terra456 <terra.456@yandex.ru>
2023-04-17 09:43:28 +0500

    fix: search bar test with store

--- src/components/searchBar/SearchBar.test.tsx
+++ src/components/searchBar/SearchBar.test.tsx
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom';
-import { cleanup, render } from '@testing-library/react';
+import { cleanup, fireEvent, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { afterEach } from 'vitest';
@@ -21,34 +22,51 @@ describe('Search Bar', () => {
     expect(conteiner.findByRole('button')).toBeDefined();
   });
 
-  // it('input focus', async () => {
-  //   render(<SearchBar {...props} />);
-  //   const input = await screen.getByRole('textbox');
-  //   expect(input).not.toHaveFocus();
-  //   input.focus();
-  //   expect(input).toHaveFocus();
-  // });
+  it('input focus', async () => {
+    const conteiner = render(
+      <Provider store={setupStore()}>
+        <SearchBar />
+      </Provider>
+    );
+    const input = await conteiner.getByTestId('search-input');
+    expect(input).not.toHaveFocus();
+    input.focus();
+    expect(input).toHaveFocus();
+  });
 
-  // it('add text to input and enable button', async () => {
-  //   render(<SearchBar {...props} />);
-  //   const btn = await screen.getByRole('button');
-  //   const input = await screen.getByRole('textbox');
-  //   expect(btn).toBeDisabled();
-  //   fireEvent.change(input, {
-  //     target: { value: 'smth' },
-  //   });
-  //   expect(btn).toBeEnabled();
-  // });
+  it('add text to input and enable button', async () => {
+    const conteiner = render(
+      <Provider store={setupStore()}>
+        <SearchBar />
+      </Provider>
+    );
+    const btn = await conteiner.getByTestId('search-btn');
+    const input = await conteiner.getByTestId('search-input');
+    expect(btn).toBeDisabled();
+    fireEvent.change(input, {
+      target: { value: 'smth' },
+    });
+    expect(btn).toBeEnabled();
+    expect(input).toHaveValue('smth');
+    await userEvent.click(btn);
+  });
 
-  // it('submit value', async () => {
-  //   const user = userEvent.setup();
-  //   render(<SearchBar {...props} />);
-  //   const btn = await screen.getByRole('button');
-  //   const input = await screen.getByRole('textbox');
-  //   expect(btn).toBeEnabled();
-  //   await user.type(input, 'smth');
-  //   await userEvent.click(btn);
-  //   expect(input).toHaveValue();
-  //   expect(btn).toBeEnabled();
-  // });
+  it('submit value into store', async () => {
+    const store = setupStore();
+    const conteiner = render(
+      <Provider store={store}>
+        <SearchBar />
+      </Provider>
+    );
+    const btn = await conteiner.getByTestId('search-btn');
+    const input = await conteiner.getByTestId('search-input');
+    expect(store.getState().FilterReducer).toEqual({ searchStr: undefined, currentPage: 1 });
+    expect(btn).toBeEnabled();
+    fireEvent.change(input, {
+      target: { value: 'new text' },
+    });
+    await userEvent.click(btn);
+    expect(input).toHaveValue('new text');
+    expect(store.getState().FilterReducer).toEqual({ searchStr: 'new text', currentPage: 1 });
+  });
 });
